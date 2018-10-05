72f40886a472c875740d93eb5420361551f98c58
Purvesh <purvesh.gandhi2008@yahoo.com.au>
2018-10-05 12:58:37 +1000

    My first Game Tic Tac Toe

--- app.js
+++ app.js
@@ -30,13 +30,29 @@ function switchTurn() {
   if (checkForWinner(turn)){
     setMessage("Congrats" + " " + turn + ", you won!");
     winner = turn;
-    } else if (turn == "X") {
+  } else if (turn == "X") {
     turn = "O";
     setMessage("It's " + turn + " 's turn");
-    } else {
+  } else {
     turn = "X";
     setMessage("It's " + turn + " 's turn");
-    }
+  }
+  checkDraw();
+}
+
+function checkDraw() {
+  if  (winner === null &&
+      getBox(1) !== "" &&
+      getBox(2) !== "" &&
+      getBox(3) !== "" &&
+      getBox(4) !== "" &&
+      getBox(5) !== "" &&
+      getBox(6) !== "" &&
+      getBox(7) !== "" &&
+      getBox(8) !== "" &&
+      getBox(9) !== "") {
+      setMessage("Game is Draw");
+  }
 }
 
 function checkForWinner(move) {
@@ -50,9 +66,9 @@ function checkForWinner(move) {
       checkRow(3,6,9, move)||
       checkRow(1,5,9, move)||
       checkRow(3,5,7, move))
-    {
+  {
       result = true;
-    }
+  }
       return result;    
 }
 
@@ -60,7 +76,7 @@ function checkRow(a, b, c, move) {
   var result = false;
   if (getBox(a) == move && getBox(b) == move && getBox(c) == move) {
       result = true;
-      }
+  }
   return result;
 }
 
